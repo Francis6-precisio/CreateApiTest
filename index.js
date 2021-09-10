f0fd2403148a0bdc4a83a0d66d3b3eec98f8e807
Francis  ABODO <abodojustin@gmail.com>
2021-09-10 09:37:30 +0100

    add CRUD

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const express = require("express");
 const app = express();
 let port = process.env.PORT || 5500;
 
+require("./models/dbConfig");
+const postsRoutes = require("./routes/postsController");
+const bodyParser = require("body-parser");
+//const mongoose = require("mongoose");
+
+//mongoose.set("useFindAndModify", false);
+
+// middleware
+app.use(bodyParser.json());
+app.use("/posts", postsRoutes);
+
 app.listen(port, () => console.log(`Server started on port: ${port}`));
